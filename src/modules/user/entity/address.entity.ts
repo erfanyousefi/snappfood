209d3cd266f9069acc7fe6350444481fe9a4475f
Erfan Yousefo <erfanyousefi.co@gmail.com>
2024-06-23 23:53:36 +0330

    create user address entity

---
+++ src/modules/user/entity/address.entity.ts
@@ -0,0 +1,34 @@
+import {EntityNames} from "src/common/enum/entity-name.enum";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import {UserEntity} from "./user.entity";
+
+@Entity(EntityNames.UserAddress)
+export class UserAddressEntity {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+  @Column()
+  title: string;
+  @Column()
+  province: string;
+  @Column()
+  city: string;
+  @Column()
+  address: string;
+  @Column({nullable: true})
+  postal_code: string;
+  @Column()
+  userId: number;
+  @CreateDateColumn({type: "timestamptz"})
+  created_at: Date;
+
+  @ManyToOne(() => UserEntity, (user) => user.addressList, {
+    onDelete: "CASCADE",
+  })
+  user: UserEntity;
+}
