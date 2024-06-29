0cc5767a8b5f440a6a7b72bd664d52157546f315
Erfan Yousefo <erfanyousefi.co@gmail.com>
2024-06-29 08:11:19 +0330

    add s3 vars to env

--- src/common/typeDefinations/env.d.ts
+++ src/common/typeDefinations/env.d.ts
@@ -8,6 +8,10 @@ namespace NodeJS {
     DB_USERNAME: string;
     DB_PASSWORD: string;
     DB_HOST: string;
-  
+    //s3
+    S3_ACCESS_KEY: string;
+    S3_SECRET_KEY: string;
+    S3_BUCKET_NAME: string;
+    S3_ENDPOINT: string;
   }
 }
