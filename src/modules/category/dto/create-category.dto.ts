b6c451a255b234f4cde8a8bdde523f3a3a3285d3
Erfan Yousefo <erfanyousefi.co@gmail.com>
2024-06-29 08:11:32 +0330

    create category dto

--- src/modules/category/dto/create-category.dto.ts
+++ src/modules/category/dto/create-category.dto.ts
@@ -1,1 +1,14 @@
-export class CreateCategoryDto {}
+import {ApiProperty, ApiPropertyOptional} from "@nestjs/swagger";
+
+export class CreateCategoryDto {
+  @ApiProperty()
+  title: string;
+  @ApiPropertyOptional({nullable: true})
+  slug: string;
+  @ApiProperty({format: "binary"})
+  image: string;
+  @ApiProperty({type: "boolean"})
+  show: boolean;
+  @ApiPropertyOptional({nullable: true})
+  parentId: number;
+}
