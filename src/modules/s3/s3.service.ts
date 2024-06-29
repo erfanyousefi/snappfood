05233119c39d209ef3dceb567dc927501cab5d36
Erfan Yousefo <erfanyousefi.co@gmail.com>
2024-06-29 08:33:38 +0330

    install multer and create upload and delete methods of s3

--- src/modules/s3/s3.service.ts
+++ src/modules/s3/s3.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from "@nestjs/common";
 import {S3} from "aws-sdk";
+import {extname} from "path";
 
 @Injectable()
 export class S3Service {
@@ -14,7 +15,22 @@ export class S3Service {
       region: "default",
     });
   }
-  async uploadFile(file: any, folderName: string){
-    
+  async uploadFile(file: Express.Multer.File, folderName: string) {
+    const ext = extname(file.originalname);
+    return await this.s3
+      .upload({
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: `${folderName}/${Date.now()}${ext}`,
+        Body: file.buffer,
+      })
+      .promise();
+  }
+  async deleteFile(Key: string) {
+    return await this.s3
+      .deleteObject({
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: decodeURI(Key),
+      })
+      .promise();
   }
 }
